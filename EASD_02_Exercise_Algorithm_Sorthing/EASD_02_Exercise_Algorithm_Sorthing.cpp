82fe2dabbc2976f06b819a58185909d36271f54b
ImamDwiYanPermana <imampermana224@gmail.com>
2023-04-07 10:42:35 +0700

    Jawaban No 2

--- EASD_02_Exercise_Algorithm_Sorthing/EASD_02_Exercise_Algorithm_Sorthing.cpp
+++ EASD_02_Exercise_Algorithm_Sorthing/EASD_02_Exercise_Algorithm_Sorthing.cpp
@@ -4,6 +4,8 @@
 // 1. Memprogram tidak hanya sekedar menulis program yang asal jadi dan asla bisa runing. 
 //    Memprogram itu perlu cara,teknik, dan metologiyang baik agar program yang di tulis program efektif
 //    Langkah lankah penyelesaian persoalan yang dinamakan algoritma adalh hal ynag paling initn dalam pemograman dan inti dari algoritma adalah logika dan sistematika 
+// 2.Struktur Data Linier: adalah struktur data yang memiliki urutan tertentu dalam penyimpanan dan aksesnya. Contohnya seperti array, linked list, queue, dan stack.
+//   Struktur Data Non - Linier: adalah struktur data yang tidak memiliki urutan atau hubungan tertentu antar elemennya.Contohnya seperti tree, graph, dan hash table.
 
 
 
