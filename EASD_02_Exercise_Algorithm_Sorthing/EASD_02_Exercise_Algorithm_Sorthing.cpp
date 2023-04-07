decf859509caa5d0dd12d0aa543107249ff49740
ImamDwiYanPermana <imampermana224@gmail.com>
2023-04-07 10:44:19 +0700

    Jawaban No 3

--- EASD_02_Exercise_Algorithm_Sorthing/EASD_02_Exercise_Algorithm_Sorthing.cpp
+++ EASD_02_Exercise_Algorithm_Sorthing/EASD_02_Exercise_Algorithm_Sorthing.cpp
@@ -6,6 +6,8 @@
 //    Langkah lankah penyelesaian persoalan yang dinamakan algoritma adalh hal ynag paling initn dalam pemograman dan inti dari algoritma adalah logika dan sistematika 
 // 2.Struktur Data Linier: adalah struktur data yang memiliki urutan tertentu dalam penyimpanan dan aksesnya. Contohnya seperti array, linked list, queue, dan stack.
 //   Struktur Data Non - Linier: adalah struktur data yang tidak memiliki urutan atau hubungan tertentu antar elemennya.Contohnya seperti tree, graph, dan hash table.
-
+// 3.Ukuran Input: Semakin besar ukuran input data yang harus diolah, semakin lama waktu yang dibutuhkan untuk menyelesaikan algoritma.
+//   Kompleksitas Algoritma : Algoritma dengan kompleksitas yang tinggi, seperti algoritma dengan nested loop, biasanya membutuhkan waktu yang lebih lama untuk dieksekusi dibandingkan algoritma dengan kompleksitas yang lebih rendah.
+//   Jenis Bahasa Pemrograman : Beberapa bahasa pemrograman lebih efisien dalam menangani tugas - tugas tertentu, sehingga dapat mengurangi waktu eksekusi program.
 
 
