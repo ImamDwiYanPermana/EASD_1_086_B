079da8039f9a751893735839fc262e2eb9d9b79d
ImamDwiYanPermana <imampermana224@gmail.com>
2023-04-07 10:41:45 +0700

    Jawaban No1

--- EASD_02_Exercise_Algorithm_Sorthing/EASD_02_Exercise_Algorithm_Sorthing.cpp
+++ EASD_02_Exercise_Algorithm_Sorthing/EASD_02_Exercise_Algorithm_Sorthing.cpp
@@ -1,9 +1,9 @@
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+//<<===Jawaban===>>
+// 1. Memprogram tidak hanya sekedar menulis program yang asal jadi dan asla bisa runing. 
+//    Memprogram itu perlu cara,teknik, dan metologiyang baik agar program yang di tulis program efektif
+//    Langkah lankah penyelesaian persoalan yang dinamakan algoritma adalh hal ynag paling initn dalam pemograman dan inti dari algoritma adalah logika dan sistematika 
+
 
 
